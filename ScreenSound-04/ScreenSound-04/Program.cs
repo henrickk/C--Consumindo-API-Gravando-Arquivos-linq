04255767d36151322e69aea21990fb033437fb1a
Henrick Adrian <81784316+henrickk@users.noreply.github.com>
2024-04-22 17:49:32 -0300

    Add files via upload

---
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using ScreenSound_04.Modelos;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try { 
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        Console.WriteLine(musicas.Count);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+    } 
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
