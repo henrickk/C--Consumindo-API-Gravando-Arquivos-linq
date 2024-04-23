8df5d597362cf293b08f33b374cafc95436d7780
Henrick Adrian <81784316+henrickk@users.noreply.github.com>
2024-04-23 18:00:26 -0300

    Att

--- ScreenSound-04/ScreenSound-04/Program.cs
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -4,15 +4,32 @@ using ScreenSound_04.Filtros;
 
 using (HttpClient client = new HttpClient())
 {
-    try { 
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
 
+ 
 
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        Console.WriteLine(musicas.Count);
+
+
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
-        LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
-    } 
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+        var minhasMusicasPreferidas = new MusicasPreferidas("Henrick");
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[6]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[14]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[20]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[22]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[31]);
+
+        minhasMusicasPreferidas.ExibirMusicasFavoritas();
+
+        minhasMusicasPreferidas.GerarArquivoJson();
+
+    }
     catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
